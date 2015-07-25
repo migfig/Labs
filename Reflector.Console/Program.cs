53e46b4cc8ca170571ef178146b15f980f9ffce2
mfigueroa <miguel.figueroa@firehost.com>
2015-07-25 17:50:44 -0500

    Improved the rendering and added support for processing a single file or an entire folder

--- Reflector.Console/Program.cs
+++ Reflector.Console/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Reflector.Console
@@ -10,8 +12,71 @@ namespace Reflector.Console
     {
         static void Main(string[] args)
         {
-            var parser = new Parser();
-            System.Console.WriteLine(parser.Render(typeof(Parser), string.Empty));
+            if(null == args && !args.Any())
+            {
+                DisplayUsage();
+                return;
+            }
+
+            var options = ParseCommandLine(string.Join(" ", args));
+
+            if(!options.Any() && (!Parser.IsFile(options["file"]) || !Parser.IsPath(options["path"])))
+            {
+                DisplayUsage();
+                return;
+            }
+
+            var parser = new Parser(
+                options.ContainsKey("file") ? options["file"] : options["path"], 
+                options.ContainsKey("include"));
+            System.Console.WriteLine(parser.Render(typeof(Parser)));
+        }
+
+        private static Dictionary<string, string> ParseCommandLine(string args)
+        {
+            var options = new Dictionary<string, string> { 
+                { "file",    @"(?<file>[a-zA-Z0-9:\\\-\.]*[\.]dll)" },
+                { "path",    @"((?<pathOption>[\-]p)[\s]*(?<path>[a-zA-Z0-9:\\\-\.]*))" },
+                { "types",   @"((?<typeOption>[\-]t)[\s]*(?<types>[a-zA-Z0-9,\.]*))" },
+                { "methods", @"((?<methodOption>[\-]m)[\s]*(?<methods>[a-zA-Z0-9,]*))" }
+            };
+            var dict = new Dictionary<string, string>();
+
+            if (args.Contains("-i"))
+            {
+                dict.Add("include", "true");
+            }
+
+            foreach(var key in options.Keys)
+            {
+                var value = Regex.Match(args, options[key]).Groups[key].Value;
+                if(!string.IsNullOrEmpty(value))
+                {
+                    if(key == "file" || key == "path")
+                    {
+                        value = Parser.BuildPath(value);
+                    }
+                    dict.Add(key, value);
+                }
+            }
+
+            return dict;
+        }
+                
+        private static void DisplayUsage()
+        {
+            System.Console.WriteLine("Builder usage:");
+            System.Console.WriteLine("{0}builder [<file>.dll] [options]", "\t");
+            System.Console.WriteLine("{0}builder [options]{1}", "\t", Environment.NewLine);
+            System.Console.WriteLine("{0}where options are:", "\t");
+            System.Console.WriteLine("{0}-p <path>", "\t\t");
+            System.Console.WriteLine("{0}specifies the path to process. example: is c:\\mybin\\ or .", "\t\t\t");
+            System.Console.WriteLine("{0}-i", "\t\t");
+            System.Console.WriteLine("{0}include .NET System.Object methods", "\t\t\t");
+            System.Console.WriteLine("{0}-t <type list>", "\t\t");
+            System.Console.WriteLine("{0}list of only types (comma separated) to process", "\t\t\t");
+            System.Console.WriteLine("{0}-m <type list>", "\t\t");
+            System.Console.WriteLine("{0}list of only methods (comma separated) to process", "\t\t\t");
         }
     }
 }
--- Reflector/Parser.cs
+++ Reflector/Parser.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
@@ -10,7 +12,90 @@ namespace Reflector
 {
     public class Parser
     {
-        public string Render(Type type, string sourceFile = "")
+        private readonly bool _includeSystemObjects;
+        private readonly string _sourcePath;
+
+        public Parser()
+            :this("", true)
+        {
+        }
+
+        public Parser(string sourcePath = "", bool includeSystemObjects = false)
+        {
+            _sourcePath = sourcePath;
+            _includeSystemObjects = includeSystemObjects;
+        }
+
+        [Description("Sample attribute")]
+        public string Render(Type[] onlyTypes = null, string[] onlyMethods = null)
+        {
+            if(string.IsNullOrEmpty(_sourcePath))
+            {
+                return string.Empty;
+            }
+
+            if(IsFile(_sourcePath))
+            {
+                return renderFile(_sourcePath, onlyTypes, onlyMethods);
+            }
+            else if(IsPath(_sourcePath))
+            {
+                var files = new StringBuilder();
+
+                foreach(var file in Directory.GetFiles(_sourcePath, "*.dll"))
+                {
+                    files.AppendFormat("{0}{1}", files.Length > 0 ? ";" : string.Empty,
+                        renderFile(file, onlyTypes, onlyMethods));
+                }
+
+                return files.ToString();
+            }
+
+            return string.Empty;
+        }
+       
+        private string renderFile(string fileName, Type[] onlyTypes = null, string[] onlyMethods = null)
+        {
+            try
+            {
+                var asm = Assembly.LoadFrom(fileName);
+                var types = asm.GetTypes();
+                if (onlyTypes != null && onlyTypes.Any())
+                {
+                    types = types.Where(t => onlyTypes.Contains(t)).ToArray();
+                }
+
+                var files = new StringBuilder();
+                foreach (var t in types)
+                {
+                    files.AppendFormat("{0}{1}", files.Length > 0 ? ";" : string.Empty,
+                        render(t, onlyTypes, onlyMethods));
+                }
+
+                return files.ToString();
+            }
+            catch (Exception)
+            {
+                return string.Empty;
+            }
+        }
+
+        public string Render(Type type)
+        {
+            return render(type, null, null);
+        }
+
+        public string Render(Type type, params string[] onlyMethods)
+        {
+            return render(type, null, onlyMethods);
+        }
+
+        public string Render(Type type, params Type[] onlyTypes)
+        {
+            return render(type, onlyTypes, null);
+        }
+
+        private string render(Type type, Type[] onlyTypes, string[] onlyMethods)
         {
             string fileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
                 type.FullName) + ".xml";
@@ -19,40 +104,63 @@ namespace Reflector
                 File.Delete(fileName);
             }
 
+            var methods = type.GetMethods();
+            if(!_includeSystemObjects)
+            {
+                var sysMethods = "ToString,Equals,GetHashCode,GetType".Split(',');
+                methods = methods.Where(m => !sysMethods.Contains(m.Name)).ToArray();
+            }
+            if(null != onlyMethods && onlyMethods.Any())
+            {
+                methods = methods.Where(m => onlyMethods.Contains(m.Name)).ToArray();
+            }
+
             using (var writer = new StreamWriter(fileName))
             {
                 var root =
                     new XElement("type",
                             new XAttribute("name", type.FullName),
+                        
                         new XElement("attributes",
-
                         from ta in type.GetCustomAttributes(true)
                         select
-                            new XElement("attribute",
-                                new XAttribute("name", ta.ToString()),
-                                new XAttribute("type", ta.GetType().FullName)
+                            new XElement("attribute",                                
+                                new XAttribute("type", ta.GetType().FullName),
+                                GetAttributeValues(ta)
                             )
                          ),
 
-                        from m in type.GetMethods()
+                        new XElement("properties",
+                        from prop in type.GetProperties()
                         select
-                            new XElement("methods",
+                            new XElement("property",
+                                new XAttribute("name", prop.Name),
+                                new XAttribute("type", prop.PropertyType.FullName)
+                            )
+                         ),
+
+                        new XElement("methods",                        
+                        from m in methods
+                        select
                                 new XElement("method",
                                     new XAttribute("name", m.Name),
                                     new XAttribute("type", m.ReturnType.FullName),
+                                    new XAttribute("public", m.IsPublic),
+                                    new XAttribute("private", m.IsPrivate),
+                                    new XAttribute("static", m.IsStatic),
 
-                        from ma in m.GetCustomAttributes(true)
+                        new XElement("attributes",
+                        from ma in m.GetCustomAttributes(true)                      
                         select
-                            new XElement("attributes",
                                 new XElement("attribute",
-                                    new XAttribute("name", ma.ToString()),
-                                    new XAttribute("type", ma.GetType().FullName)
+                                    new XAttribute("type", ma.GetType().FullName),
+                                    GetAttributeValues(ma)
                                 )
                             ),
-
+                            
+                        new XElement("parameters",
                         from p in m.GetParameters()
                         select
-                            new XElement("parameters",
                                 new XElement("parameter",
                                     new XAttribute("name", p.Name),
                                     new XAttribute("type", p.ParameterType.FullName)
@@ -62,15 +170,55 @@ namespace Reflector
                     )
                 );
 
-                if (!string.IsNullOrEmpty(sourceFile))
+                if (!string.IsNullOrEmpty(_sourcePath))
                 {
-                    root.Add(new XAttribute("source", sourceFile));
+                    root.Add(new XAttribute("source", _sourcePath));
                 }
 
                 root.Save(writer);
             }
 
             return fileName;
+        }
+
+        private XElement GetAttributeValues(object attribute)
+        {
+            return
+                new XElement("properties",
+                        from prop in attribute.GetType().GetProperties()
+                        let value = prop.GetValue(attribute) ?? ""
+                        select
+                            new XElement("property",
+                                new XAttribute("name", prop.Name),
+                                new XAttribute("type", prop.PropertyType.FullName),
+                                new XAttribute("value", value.ToString())
+                            )
+                         );
+        }
+
+        public static bool IsFile(string fileName)
+        {
+            return File.Exists(fileName)
+                && Path.GetExtension(fileName).ToLower() == "dll";
+        }
+
+        public static bool IsPath(string path)
+        {
+            return Directory.Exists(BuildPath(path));
+        }
+
+        public static string BuildPath(string path)
+        {
+            if (path == ".")
+            {
+                return Environment.CurrentDirectory;
+            }
+            else if (Path.GetDirectoryName(path) == "")
+            {
+                return Path.Combine(Environment.CurrentDirectory, path);
+            }
+
+            return path;
         }
     }
 }
