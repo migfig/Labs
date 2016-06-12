8aec6cc34015082983922f82ee2c7e40cce873ff
migfig <micky.figueroa@hotmail.com>
2016-06-11 20:46:39 -0500

    Simplified finding a code item

--- Log.Visor.VStudio/LogViewer.cs
+++ Log.Visor.VStudio/LogViewer.cs
@@ -29,6 +29,7 @@ namespace Log.Visor.VStudio
     [Guid("02bee7af-68af-40ec-8e32-ea7a53105041")]
     public class LogViewer : ToolWindowPane
     {
+        private EnvDTE.OutputWindowPane _outputPane = null;
         /// <summary>
         /// Initializes a new instance of the <see cref="LogViewer"/> class.
         /// </summary>
@@ -47,60 +48,32 @@ namespace Log.Visor.VStudio
         {
             var found = false;
             var errMsg = string.Empty;
+            var progId = e.ProgId;
             try
             {
-                var vsInstance = Marshal.GetActiveObject(e.ProgId);
+                var vsInstance = Marshal.GetActiveObject(progId);
                 if (null != vsInstance)
                 {
                     var dte = (EnvDTE80.DTE2)vsInstance;
-                    var output = dte.ToolWindows.OutputWindow.ActivePane;
-                    var solution = dte.Solution;
-                    if (null != solution)
+                    var dte90 = (EnvDTE80.Solution2)dte.Solution;
+                    try
                     {
-                        var projects = from project in solution.Projects.Cast<EnvDTE.Project>()
-                                       where project.Name.Split('\\').First()
-                                            .Equals(e.NameSpace.Split('\\').First())
-                                       select project;
-                        if (projects.Any())
-                        {
-                            var items = from project in projects
-                                        from pi in project.ProjectItems.Cast<EnvDTE.ProjectItem>()
-                                        let item = pi.Name.Equals(e.ClassName)
-                                            ? pi
-                                            : FindItem(output, pi.ProjectItems == null
-                                                ? null
-                                                : pi.ProjectItems.Cast<EnvDTE.ProjectItem>(), e)
-                                        where item != null
-                                        select item;
-
-                            if (items.Count() == 1)
-                            {
-                                found = OpenItem(output, items.First(), e);
-                            }
-                            else
-                            {
-                                Log(output, string.Format("Looking for Code item in {0} projects. Namespace {1}", items.Count(), e.NameSpace));
-                                foreach (var item in items)
-                                {
-                                    var parts = e.NameSpace.Split('\\');
-                                    for (var i = parts.Length - 1; i >= 0; i--)
-                                    {
-                                        var name = string.Join("\\", parts, 0, i);
-                                        Log(output, string.Format("Looking for Code item {0} as named {1}", i, name));
-                                        if (item.ContainingProject.Name.Equals(name))
-                                        {
-                                            found = OpenItem(output, item, e);
-                                            break;
-                                        }
-                                    }
-                                    if (found) break;
-                                }
-                            }
-                        }
+                        _outputPane = dte.ToolWindows.OutputWindow.OutputWindowPanes.Item("LogVisorX");
                     }
-                    else
+                    catch (Exception)
                     {
-                        errMsg = "Solution not available";
+                        _outputPane = dte.ToolWindows.OutputWindow.OutputWindowPanes.Add("LogVisorX");
+                    }
+                    if (_outputPane != null)
+                    {
+                        _outputPane.Activate();
+                        _outputPane.Clear();
+                    }
+
+                    var prjItem = dte90.FindProjectItem(e.ClassName);
+                    if (prjItem != null)
+                    {
+                        found = OpenItem(prjItem, e);
                     }
                 }
                 else
@@ -110,7 +83,7 @@ namespace Log.Visor.VStudio
             }
             catch (Exception ex)
             {
-                errMsg = ex.Message + ex.StackTrace;
+                errMsg = ex.Message + " ProgId [" + progId + "] " + ex.StackTrace;
             }
 
             if (errMsg.Length > 0)
@@ -119,16 +92,16 @@ namespace Log.Visor.VStudio
             }
             else
             {
-                if(!found)
+                if (!found)
                 {
                     MessageBox.Show(string.Format("Class {0} not found in namespace {1}.", e.ClassName, e.NameSpace), "Code Item not found!", MessageBoxButton.OK, MessageBoxImage.Exclamation);
                 }
             }
         }
 
-        private bool OpenItem(EnvDTE.OutputWindowPane pane, EnvDTE.ProjectItem item, ViewCodeArgs e)
+        private bool OpenItem(EnvDTE.ProjectItem item, ViewCodeArgs e)
         {
-            Log(pane, "Code item found [" + item.Name + "] in project [" + item.ContainingProject.Name + "]");
+            Log("Code item found [" + item.Name + "] in project [" + item.ContainingProject.Name + "]");
             var window = item.Open();
             if (null != window)
             {
@@ -139,31 +112,14 @@ namespace Log.Visor.VStudio
 
             return true;
         }
-        private EnvDTE.ProjectItem FindItem(EnvDTE.OutputWindowPane pane, IEnumerable<EnvDTE.ProjectItem> items, ViewCodeArgs e)
-        {
-            if (items == null || !items.Any()) return null;
 
-            var item = items.FirstOrDefault(x => x.Name.Equals(e.ClassName));
-            if (item != null) return item;
-
-            foreach (var i in items.Where(x => x.Kind.Equals(EnvDTE.Constants.vsProjectItemKindPhysicalFolder)
-                            || x.Kind.Equals(EnvDTE.Constants.vsProjectItemKindVirtualFolder)))
-            {
-                Log(pane, string.Format("looking for class {0} in project {1} at folder {2}", e.ClassName, i.ContainingProject.Name, i.Name));
-                var itemx = FindItem(pane, i.ProjectItems.Cast<EnvDTE.ProjectItem>(), e);
-                if (itemx != null)
-                    return itemx;
-            }
-
-            return null;
-        }
-
-        private void Log(EnvDTE.OutputWindowPane pane, string message)
+        private void Log(string message)
         {
             try
             {
-                pane.OutputString(message + Environment.NewLine);
-            } catch(Exception) {;} 
+                _outputPane.OutputString(message + Environment.NewLine);
+            }
+            catch (Exception) {; }
         }
     }
 }
