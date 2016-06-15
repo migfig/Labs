e8696e05d265e86ab04968585f4b404fdfb7851b
migfig <micky.figueroa@hotmail.com>
2016-06-15 18:56:46 -0500

    Added Trainer UWP app

---
+++ Trainer/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using Trainer.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace Trainer.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
