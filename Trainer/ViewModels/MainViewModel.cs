98667649ad92e154c91ac0f6ace2b7988617284b
migfig <micky.figueroa@hotmail.com>
2016-08-05 21:25:54 -0500

    added default presentation

--- Trainer/ViewModels/MainViewModel.cs
+++ Trainer/ViewModels/MainViewModel.cs
@@ -5,6 +5,7 @@ using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using Template10.Mvvm;
 using Trainer.Domain;
+using Windows.Storage;
 
 namespace Trainer.ViewModels
 {
@@ -31,10 +32,32 @@ namespace Trainer.ViewModels
             using (var service = ApiServiceFactory.CreateService<Presentation>())
             {
                 _presentations = new ObservableCollection<Presentation>(await service.GetItems("presentations"));
+                if(!_presentations.Any())
+                {
+                    var defaultPresentation = await LoadDefaultPresentation();
+                    if(defaultPresentation != null)
+                    {
+                        _presentations.Add(defaultPresentation);
+                    }
+                }
                 action.Invoke(_presentations);
             }
         }
 
         public string SelectedPresentationTitle { get; set; }
+
+        private async Task<Presentation> LoadDefaultPresentation()
+        {
+            try
+            {
+                var file = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Presentations/default.xml"));
+                string xml = await FileIO.ReadTextAsync(file);
+                return Common.XmlHelper2<Presentation>.LoadFromString(xml);
+            }
+            catch(Exception)
+            {
+                return null;
+            }
+        }
     }
 }
