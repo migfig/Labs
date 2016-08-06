a7c9da1892aecb8bbd4ff1f7227c498097007836
migfig <micky.figueroa@hotmail.com>
2016-08-06 13:57:14 -0500

    removed unnecessary code and completed default presentation

--- Trainer/ViewModels/MainPageViewModel.cs
+++ Trainer/ViewModels/MainPageViewModel.cs
@@ -54,19 +54,13 @@ namespace Trainer.ViewModels
         {
             args.Cancel = false;
             await Task.CompletedTask;
-        }
-
-        public void GotoDetailsPage() =>
-            NavigationService.Navigate(typeof(Views.DetailPage), "");
+        }        
 
         public void GotoSettings() =>
             NavigationService.Navigate(typeof(Views.SettingsPage), 0);
 
-        public void GotoPrivacy() =>
-            NavigationService.Navigate(typeof(Views.SettingsPage), 1);
-
         public void GotoAbout() =>
-            NavigationService.Navigate(typeof(Views.SettingsPage), 2);
+            NavigationService.Navigate(typeof(Views.SettingsPage), 1);
 
         public void GotoSlides(string title) =>
             NavigationService.Navigate(typeof(Views.SlidePage), new { Title = title });
