2c659898ca534b19c4d69f369d32695d231794bb
migfig <micky.figueroa@hotmail.com>
2016-08-26 20:14:51 -0500

    added slides control to edit slides

---
+++ Trainer/ViewModels/SlidesPageViewModel.cs
@@ -0,0 +1,160 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Template10.Mvvm;
+using Template10.Services.NavigationService;
+using Trainer.Domain;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Navigation;
+
+namespace Trainer.ViewModels
+{
+    public class SlidesPageViewModel : ViewModelBase
+    {
+        private readonly ICodeServices _codeServices;
+
+        public SlidesPageViewModel()
+        {
+            if (Windows.ApplicationModel.DesignMode.DesignModeEnabled)
+            {
+            }
+
+            _codeServices = new CodeServices();
+            MainViewModel.ViewModel.GetPresentations((items) => Presentations = items);
+        }
+
+        public string Foother
+        {
+            get { return string.Format("Slide {0} of {1}", Presentation.Slide.IndexOf(CurrentSlide) + 1, Presentation.Slide.Count); }
+        }
+
+        private ObservableCollection<Presentation> _presentations;
+        public ObservableCollection<Presentation> Presentations
+        {
+            get
+            {
+                return _presentations;
+            }
+
+            private set
+            {
+                Set(ref _presentations, value);
+                Presentation = string.IsNullOrEmpty(MainViewModel.ViewModel.SelectedPresentationTitle)
+                    ? _presentations.FirstOrDefault()
+                    : _presentations.FirstOrDefault(x => x.Title.Equals(MainViewModel.ViewModel.SelectedPresentationTitle));
+            }
+        }
+
+
+        private Presentation _presentation;
+        public Presentation Presentation
+        {
+            get
+            {
+                return _presentation;
+            }
+            private set
+            {
+                Set(ref _presentation, value);
+                CurrentSlide = _presentation.Slide.FirstOrDefault();
+            }
+        }
+
+        private Slide _currentSlide;
+        public Slide CurrentSlide
+        {
+            get { return _currentSlide; }
+            private set { Set(ref _currentSlide, value); }
+        }
+
+        public string BackgroundImageUrl
+        {
+            get { return !string.IsNullOrEmpty(CurrentSlide.Image) ? CurrentSlide.Image : Presentation.Image; }
+        }
+
+        public override async Task OnNavigatedToAsync(object parameter, NavigationMode mode, IDictionary<string, object> suspensionState)
+        {
+            if(null != parameter)
+            {
+                if (parameter is string)
+                {
+                    CurrentSlide = _presentation.Slide.First(x => x.Title.Equals(parameter.ToString()));
+                }
+                else //is dynamic object
+                {
+                    Presentation = _presentations
+                        .FirstOrDefault(x => x.Title.Equals(((dynamic)parameter).Title));
+                }
+            }
+
+            await Task.CompletedTask;
+        }
+
+        public override async Task OnNavigatedFromAsync(IDictionary<string, object> suspensionState, bool suspending)
+        {
+            if (suspending)
+            {
+                suspensionState[nameof(CurrentSlide.Title)] = CurrentSlide.Title;
+            }
+            await Task.CompletedTask;
+        }
+
+        public override async Task OnNavigatingFromAsync(NavigatingEventArgs args)
+        {
+            args.Cancel = false;
+            await Task.CompletedTask;
+        }
+
+        public void GotoNextSlide() =>
+            NavigationService.Navigate(typeof(Views.SlidePage), GetNextSlideTitle());
+
+        public void GotoPreviousSlide() =>
+            NavigationService.Navigate(typeof(Views.SlidePage), GetPreviousSlideTitle());
+
+        public void CopyCode() =>
+            _codeServices.CopyCode(CurrentSlide.Component);
+
+        public void ViewCode() =>
+            _codeServices.ViewCode(CurrentSlide.Component);
+
+        public Visibility CopyCodeVisibility { get { return CurrentSlide.Component.Any(x => x.IsBrowsable && x.Action.Equals(ComponentAction.Copy))                     
+                        ? Visibility.Visible : Visibility.Collapsed; } }
+
+        public Visibility ViewCodeVisibility
+        {
+            get
+            {
+                return CurrentSlide.Component.Any(x => x.IsBrowsable && x.Action.Equals(ComponentAction.View))
+                    ? Visibility.Visible : Visibility.Collapsed;
+            }
+        }
+
+        public bool CanGotoPreviousSlide { get { return !_currentSlide.Title.Equals(_presentation.Slide.First().Title); } }
+
+        public bool CanGotoNextSlide { get { return !_currentSlide.Title.Equals(_presentation.Slide.Last().Title); } }
+
+        private string GetNextSlideTitle()
+        {
+            var index = _presentation.Slide.IndexOf(_currentSlide);
+            if(index < _presentation.Slide.Count-1)
+            {
+                return _presentation.Slide.ElementAt(index + 1).Title;
+            }
+
+            return _presentation.Slide.First().Title;
+        }
+
+        private string GetPreviousSlideTitle()
+        {
+            var index = _presentation.Slide.IndexOf(_currentSlide);
+            if (index > 0)
+            {
+                return _presentation.Slide.ElementAt(index - 1).Title;
+            }
+
+            return _presentation.Slide.First().Title;
+        }
+    }
+}
+
---
+++ Trainer/Views/SlidesPage.xaml.cs
@@ -0,0 +1,15 @@
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace Trainer.Views
+{
+    public sealed partial class SlidesPage : Page
+    {
+        public SlidesPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
